650f85abc617002192d8cf9ff20e0fa9e7dd2622
espedroso <espgordo@gmail.com>
2024-01-30 18:53:34 -0300

    create javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const searchInput= document.getElementById('search-input');
+const resultsartists = document.getElementById('result-artist');
+const resultPlayList = documnet.getElementById('result-playlists');
+
+function requestApi(searchTerm){
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+    .then({response} => response.json())
+    .then({result} => displayResults(result))
+};
+
+document.addEventListener('input', function(){
+    const searchTerm = searchInput.ariaValueMax.toLowerCase();
+    if (searchTerm === ''){
+        resultPlayList.classList.add('hidden');
+        resultsartists.classList.remove('hidden');
+        return;
+    }
+})
